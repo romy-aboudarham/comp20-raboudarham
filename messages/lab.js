b2b495bd39a93c412e685372db0bb866a1f2326c
ROMY <tuftscs@tuftscsvm.tuftscsvm>
2016-03-09 16:00:30 -0500

    test commit

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,16 @@
 // Your JavaScript goes here...
+
+
+function parse() {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+        var messages = JSON.parse(request.responseText);
+        if (request.readyState == 4 && request.status == 200) {
+        elem = document.getElementById("messages");
+        for(var count = 0; count < messages.length; count++) {
+       		elem.innerHTML += "<p>" + messages[count].content + " " + messages[count].username + "</p>";
+    	}
+	}
+	request.send();
+}
+}
